1e419f6af6027e66e914276888ea6ab56fd3434c
rjlmota <rjlmota@gmail.com>
2018-04-22 22:49:11 -0300

    Turning the Servers into Class

---
+++ californium-core/src/main/java/org/eclipse/californium/core/observe/Event.java
@@ -0,0 +1,96 @@
+package org.eclipse.californium.core.observe;
+
+
+//import org.eclipse.californium.core.CoapObserveRelation;
+
+import java.net.InetAddress;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.eclipse.californium.core.observe.Server;
+
+import org.eclipse.californium.core.coap.Message;
+
+//import org.eclipse.californium.core.CoapClient;
+//import org.eclipse.californium.core.CoapResponse;
+ 
+
+
+
+public class Event {
+	
+	public static Set <InetAddress> obs_arr = new HashSet <InetAddress>();
+	private static int observers = 0;
+	public static List <Server> Servers = new ArrayList <Server>();
+	
+	
+	
+	public static void add_data(Timestamp last_Time, InetAddress inetAddress) {
+		for(int i = 0; i < Servers.size(); i++) {
+			if(inetAddress.equals(Servers.get(i).IP)){
+				Servers.get(i).last_datetime = last_Time;
+				Servers.get(i).rec_msgs++;
+				event_data();
+				return;
+			}	
+		}
+		Servers.add(new Server());
+		Servers.get(Servers.size()-1).IP = inetAddress;
+		Servers.get(Servers.size()-1).last_datetime = last_Time;
+		event_data();
+	}
+	
+	
+	private static int most_recent_event() {
+		int index = 0;
+		long higher = 0;
+		for (int i = 0; i < Servers.size(); i++) 
+			if(Servers.get(i).last_datetime.getTime() > higher) { 
+				higher = Servers.get(i).last_datetime.getTime();
+				index = i;
+			}
+		return index;
+	}
+		
+	private static void event_data() {
+		observers = 0;
+		
+		if(Servers.size() < 2){
+			System.out.println(observers + " mote is observing the event \n");
+			return;
+		}
+		for(int i = 0; i < Servers.size(); i++)
+				if(Math.abs(Servers.get(most_recent_event()).last_datetime.getTime() - Servers.get(i).last_datetime.getTime()) < 10000) {
+					observers++;
+					obs_arr.add(Servers.get(i).IP);
+				}
+		System.out.println(observers + " mote(s) observing the event \n");
+	}
+	
+	
+	
+	
+	public static String next_con(Message message) {
+		List<InetAddress> obs_list = new ArrayList<>(obs_arr);
+		int next = 0;
+		for(int i = 0; i < obs_list.size(); i++){ 
+			if (message.getSource().equals(obs_list.get((i)))) {
+				
+				if(message.isConfirmable()) {
+					Servers.get(i).last_con =  message.getMID();
+				}
+				
+				next = Servers.get(i).last_con + observers + i;
+			
+		
+				System.out.println("Ultima CON foi: " + Servers.get(i).last_con + " Proxima CON deveria ser: " + next);
+			
+				return Integer.toString(next);
+			}
+				
+		}
+	return "fail";
+	}
+}
---
+++ californium-core/src/main/java/org/eclipse/californium/core/observe/Server.java
@@ -0,0 +1,12 @@
+package org.eclipse.californium.core.observe;
+
+import java.net.InetAddress;
+import java.sql.Timestamp;
+
+public class Server{
+	 public InetAddress IP;
+	 public Timestamp last_datetime;
+	 public int last_con;
+	 public int rec_msgs = 0;
+	
+}
